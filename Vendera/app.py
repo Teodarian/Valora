4198a3a49bfd7888b436588d141448822855c5b0
Kaushik7244 <Kaushik7244@gmail.com>
2026-06-10 23:49:55 +0200

    wejdhd

--- Vendera/app.py
+++ Vendera/app.py
@@ -48,4 +48,4 @@ if __name__ == "__main__":
     print("BASE_DIR:", BASE_DIR)
     print("Template folder:", app.template_folder)
     print("Static folder:", app.static_folder)
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
